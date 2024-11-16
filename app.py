2817b9b595e6e95033190ba944c92064989e411b
Cassidy <cassidy.reyes@csedge.org>
2024-11-16 13:25:50 -0800

    FInished application

--- app.py
+++ app.py
@@ -2,18 +2,6 @@ from tinydb import TinyDB, Query
 # Creating an instance VVV
 db = TinyDB('./db.json')
 
-# db.insert({'Subject': 'Math', 'Color': 'Red'})
-# user = Query()
-# onedata = db.search(user.Subject == 'Math')
-# alldata = db.all()
-# add =  db.insert({'Subject': 'Math', 'Color': 'Red'})
-# create(insert function)
-# read(calling db.all/db.search)
-
-#OPT delete = db.remove(user.Subject == 'Math')
-#OPT update = db.update({'Subject': 'ELA}, user.Subject == 'ERWC')
-#OPT update()
-#OPT delete(db.remove)
 todo = Query()
 
 def create():
@@ -22,7 +10,6 @@ def create():
     print('Add a description to your task:')
     descinput = input()
     db.insert({'Name': str(createinput), 'Desc': str(descinput)})
-
 def read():
     print('Would you like to see all your tasks, or just one?(all, one):')
     allorone = input()
@@ -35,7 +22,9 @@ def read():
         taskwanted = input()
         taskfind = db.search(todo.Name == str(taskwanted))
         print(taskfind)
-
+    else:
+        print('That is not a valid command, choose from the list within the parenthesis/().')
+        print()
 def update():
     print('Which task would you like to update?:')
     print('(NOTE - this is case sensitive!!!!)')
@@ -58,21 +47,37 @@ def update():
         db.update({'Name': str(newname), 'Desc': str(newdesc)}, todo.Name == taskupdate)
     else:
         print('That is not a valid command, choose from the list within the parenthesis/().')
-
+        print()
 def delete():
     print('Would you like to delete one task, or multiple?(all, one):')
     allorone = input()
-    
-
+    if allorone == 'all':
+        print('Your To-Do-List has been emptied.')
+        db.truncate()
+        print(db.all)
+    elif allorone == 'one':
+        print('What task would you like to be delete?:')
+        print('(NOTE - this is case sensitive!!!!)')
+        deletename = input()
+        db.remove(todo.Name == str(deletename))
+    else:
+        print('That is not a valid command, choose from the list within the parenthesis/().')
+        print()
 while True:
+    print()
+    print('Welcome to your To-Do-List!')
     userinput = input('Enter your command(create, read, update, delete): ')
     if userinput == 'create':
+        print()
         create()
     elif userinput == 'read':
+        print()
         read()
     elif userinput == 'update':
+        print()
         update()
     elif userinput == 'delete':
+        print()
         delete()
     else:
         print('That is not a valid command, choose from the list within the parenthesis/().')
